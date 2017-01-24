96f007b5944bfd102a1ed0fe796ba4b18b41aa4c
Samuel Bancroft <samuel.bancroft@scarrowhill.co.uk>
2017-01-24 16:26:04 +0000

    Added plots, periodogram into analyser.py

--- analyser.py
+++ analyser.py
@@ -1,8 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import signal
 
 data = np.loadtxt('tp.csv', delimiter=',')
 time, amplitude = data[:,0], data[:,1]
 
 plt.plot(time, amplitude)
+plt.show()
+
+ps = np.abs(np.fft.fft(amplitude))**2
+
+f, Pxx_den = signal.periodogram(amplitude, 1000)
+plt.semilogy(f, Pxx_den)
+plt.ylim([1e-7, 1e2])
+plt.xlabel('frequency [Hz]')
+plt.ylabel('PSD [V**2/Hz]')
 plt.show()
