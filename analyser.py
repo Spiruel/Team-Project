28e11bf8c226840dd3502818c98914710e47fc85
Samuel Bancroft <spiruel@gmail.com>
2017-02-02 14:23:44 +0000

    Changes to analyser.py

--- analyser.py
+++ analyser.py
@@ -1,10 +1,60 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
+from scipy.fftpack import fft
 
-data = np.loadtxt('Team 17/sin wave test.csv', delimiter=',')
+from STFT import STFT
+
+data = np.loadtxt('Team 17/12V motor x axis ten minutes.csv', delimiter=',', comments='#')
 time, amplitude = data[:,0], data[:,1]
 
+freq = 1500
+tim_per = 2
+
+step = tim_per*freq
+
+plt.ion()
+
+t, a = np.array([0,1]), np.array([0,1])
+line, = plt.plot(t, a, 'k')
+for i in range(0,len(amplitude),1):
+	t, a = time[0:i+step], amplitude[0:i+step]
+	#plt.plot(t[-2:], a[-2:], 'k')
+
+	line.set_xdata(t); line.set_ydata(a)
+	plt.draw()
+	plt.pause(0.05)
+
+	'''scroll_size = .1
+	xlim = t[-1]
+	if xlim<=scroll_size:
+		plt.xlim([0, scroll_size])
+	else:
+		plt.xlim([xlim-scroll_size, xlim])'''
+
+
+def animate_waterfall(freq=1500, tim_per=2):
+	step = tim_per*freq
+
+	plt.ion()
+	#result = np.array([], ndim=3)
+	for i in range(0,len(amplitude),step):
+		result = STFT(amplitude[0:i+step], fs=freq)
+		img = plt.imshow(result.T, origin='lower', cmap='jet', interpolation='nearest', aspect='auto', vmax=-100)
+
+		if i == 0: plt.colorbar()
+		plt.pause(0.05)
+		#plt.savefig('test/'+str(i)+'.png')
+
+		scroll_size = 200
+		xlim = result.shape[0]
+		if xlim<=scroll_size:
+			plt.xlim([0, scroll_size])
+		else:
+			plt.xlim([xlim-scroll_size, xlim])
+
+
+'''
 plt.plot(time, amplitude)
 plt.show()
 
@@ -16,3 +66,4 @@ plt.ylim([1e-7, 1e2])
 plt.xlabel('frequency [Hz]')
 plt.ylabel('PSD [V**2/Hz]')
 plt.show()
+'''
