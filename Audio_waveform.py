153c0556f9d27490cae4305bfcd21a87d73eea2d
Teodor Tzokov <teodortzokov@Teodors-MBP.home>
2017-02-02 19:50:22 +0000

    Updated STFT again and added Audio_waveform

---
+++ Audio_waveform.py
@@ -0,0 +1,21 @@
+from __future__ import division
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import signal
+
+def waveform(data, fs = 1500):
+	'''data is 1d array of amplitudes for a given time. fs is sampling frequency of the signal'''
+	data_len = len(data)
+	time_array = np.arange(data_len)/fs
+	return (time_array,data)
+	
+	
+	
+if __name__ == '__main__':
+
+	data = np.loadtxt('Team 17/12V motor x axis ten minutes.csv', delimiter=',', comments='#')[:,1][:5000]
+	times = waveform(data)[0]
+	print times
+	print data
+	plt.plot(times, data)
+	plt.show()
