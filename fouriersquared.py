52f46bf29ea0a2e2f8ee8dae960c4a5f1f5c6761
Alexander Bell <Alexander Bell>
2017-02-06 15:27:09 +0000

    code now implements fft correctly

--- fouriersquared.py
+++ fouriersquared.py
@@ -1,3 +1,35 @@
+import matplotlib.pyplot as plt
 import numpy as np
+from scipy import signal
+from scipy.fftpack import fft
 
-function_here(x, y):
+from STFT import STFT
+
+data = np.loadtxt('Team 17/sin wave test.csv', delimiter=',', comments='#')
+time, amplitude = data[:,0], data[:,1]
+
+sample_rate = 1500.0
+sample_interval = 1.0/1500.0
+
+#def waveform(data, sample_rate):
+#    data_len = len(data)
+#    time_values = data_len / sample_rate
+#    waveform = np.array(data, time_values)
+#    return waveform
+
+def plotFourierSquared(amplitude, sample_rate):
+    n = len(time) #length of signal
+    k = np.arange(n)
+    T = n / sample_rate
+    frequency = k / T
+    frequency = frequency[range(n/2)]
+    
+    Y = fft(amplitude) / n
+    Y = Y[range(n/2)]
+    
+    plt.plot(frequency, abs(Y))
+    plt.xlabel('Freq (Hz)')
+    plt.ylabel('|Y(freq)|')
+    
+plotFourierSquared(amplitude, sample_rate)
+plt.show()
