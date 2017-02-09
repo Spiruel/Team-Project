6a40f1f63f7343fd0f1b0648d923e2cd0d79dfab
Teodor Tzokov <teodortzokov@student-10-245-139-196.wlan.dur.ac.uk>
2017-02-09 16:23:41 +0000

    Filter.py now contains both low_pass and high_pass filters

--- Find_peaks.py
+++ Find_peaks.py
@@ -2,8 +2,10 @@ from __future__ import division
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
+import peakutils
+import Fourier
     
-def find_peaks(freq_amps, fs=3000):
+def find_peak_old(freq_amps, fs=3000):
 	'''Takes inputs:
 	freq_amps -- array of amplitudes corresponding to certain frequencies in the fourier plot
 	fs -- sampling frequency
@@ -22,7 +24,13 @@ def find_peaks(freq_amps, fs=3000):
 	
 	peak_freqs = peak_indices/length*fs/2
 	
-	return (peak_freqs, peak_amps)
+	return (peak_freqs, peak_amps)	
+	
+def find_peaks(data,fs=3000):
+	fourier = Fourier.fourier(data,fs)
+	
+	freqs = fourier[0]
+	freq_amps = fourier[1]
 	
 if __name__ == '__main__':
 	
@@ -36,13 +44,24 @@ if __name__ == '__main__':
 	
 	freqs = fourier[0]
 	freq_amps = fourier[1]
-	freq_amps = 20*np.log10(freq_amps)
+	#freq_amps = 20*np.log10(freq_amps)
 	
-	peaks = find_peaks(freq_amps, fs)
+	'''peaks = find_peaks(freq_amps, fs)
 	
 	peak_freqs = peaks[0]
 	peak_amps = peaks[1]
 	
 	plt.plot(freqs,freq_amps)
 	plt.plot(peak_freqs,peak_amps, 'ro', markersize = 10)
+	plt.show()'''
+	
+	indexes = peakutils.indexes(freq_amps, thres=0.0000001, min_dist=30)
+	print indexes
+	peak_freqs = freqs[indexes] 
+	peak_amps = freq_amps[indexes]
+	plt.figure(figsize=(10,6))
+	plt.plot(peak_freqs, peak_amps,'ro', markersize = 10)
+	
+	plt.plot(freqs, freq_amps)
 	plt.show()
+	
