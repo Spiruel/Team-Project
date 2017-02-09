3d6395200dc33559e7c34ea71a841037d343d133
Teodor Tzokov <teodortzokov@student-10-245-139-196.wlan.dur.ac.uk>
2017-02-09 14:28:35 +0000

    added new low pass filter

--- Find_peaks.py
+++ Find_peaks.py
@@ -4,9 +4,16 @@ import numpy as np
 from scipy import signal
     
 def find_peaks(freq_amps, fs=3000):
+	'''Takes inputs:
+	freq_amps -- array of amplitudes corresponding to certain frequencies in the fourier plot
+	fs -- sampling frequency
+	
+	returns:
+	peak_freqs -- array containing the frequency of the peaks found
+	peak_amps -- array containing the corresponding amplitudes of the peaks'''
 	length = len(freq_amps)
 	peak_width_range = np.arange(length/1000,length/10, length/1000)
-	peak_indices = np.array(signal.find_peaks_cwt(freq_amps, peak_width_range, min_snr = 2))
+	peak_indices = np.array(signal.find_peaks_cwt(freq_amps, peak_width_range, min_snr = 3))
 	
 	peak_amps = np.array([])
 	for i in range(len(peak_indices)):
@@ -20,17 +27,22 @@ def find_peaks(freq_amps, fs=3000):
 if __name__ == '__main__':
 	
 	data = np.loadtxt('Team 17/12V motor x axis ten minutes.csv', delimiter=',', comments='#')[:,1][:5000]
+	
 	fs = 1500
 	
 	import Fourier
 	
-	freqs = Fourier.fourier(data,fs)[0]
-	freq_amps = Fourier.fourier(data, fs)[1]
+	fourier = Fourier.fourier(data,fs)
+	
+	freqs = fourier[0]
+	freq_amps = fourier[1]
 	freq_amps = 20*np.log10(freq_amps)
 	
-	peak_freqs = find_peaks(freq_amps, fs)[0]
-	peak_amps = find_peaks(freq_amps, fs)[1]
+	peaks = find_peaks(freq_amps, fs)
+	
+	peak_freqs = peaks[0]
+	peak_amps = peaks[1]
 	
 	plt.plot(freqs,freq_amps)
-	plt.plot(peak_freqs,peak_amps, 'ro', markersize = 30)
+	plt.plot(peak_freqs,peak_amps, 'ro', markersize = 10)
 	plt.show()
