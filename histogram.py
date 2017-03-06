09036b02a3b8d7254eaff8f815b01577a688b6d8
Samuel Bancroft <samuel.bancroft@scarrowhill.co.uk>
2017-03-06 15:32:37 +0000

    Added histogram test updated gitignore

---
+++ histogram.py
@@ -0,0 +1,66 @@
+# import modules and initialize plotting parameters
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt(r'D:\Users\Samuel\Dropbox\TracerCo project team folder\12V motor\specific_anomaly_test1.csv', comments=['#','\"'], delimiter=',', usecols=0)
+all_data = np.array(data)
+# lets take half of the input data for training, and half for testing
+# and create numpy arrays
+half = int(len(data)/2)
+train_data = all_data[:half]
+test = all_data[half:]
+
+#plt.figure(figsize=(16, 10))
+plt.plot(train_data)
+plt.xlabel('Amplitude /mV')
+plt.ylabel('Count')
+plt.show()
+
+bins = 100
+ordered = sorted(train_data)
+div = len(ordered) // bins
+chunks = [ ordered[int(round(div * i)): int(round(div * (i + 1)))] for i in range(bins) ]
+meds = [np.median(chunk) for chunk in chunks]
+
+n, bins, patches = plt.hist(ordered,100)
+plt.grid()
+plt.xlabel('Samples')
+plt.ylabel('Amplitude')
+
+plt.show()
+
+print('Minumum reading = {} mV., maximum = {} mV.'.format(np.amin(ordered), np.amax(ordered)))
+print('Mean = {:0.2f} mV.'.format(np.mean(ordered)))
+print('Standard deviation = {:0.2f} mV.'.format(np.std(ordered)))
+
+# define a function to return the nearest value in a list
+def find_nearest(array,value):
+    idx = (np.abs(array-value)).argmin()
+    return array[idx]
+	
+reconstructed = [find_nearest(meds, val) for val in train_data]
+error = train_data - reconstructed
+
+plt.style.use('seaborn-white')
+f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True)
+ax1.plot(train_data, 'b', lw=1, label="Original")
+ax2.plot(reconstructed, 'orange', lw=1, label="Reconstructed")
+ax3.plot(error, 'r', lw=1, label="Reconstruction Error")
+# Fine-tune figure; make subplots close to each other and hide x ticks for
+# all but bottom plot.
+f.subplots_adjust(hspace=0)
+plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
+	
+# plt.ylim([-1-0.2,1+0.2])
+for a in [ax1, ax2, ax3]:
+	a.legend(frameon=False, loc='lower right')
+ax3.set_xlabel('Samples')
+ax2.set_ylabel('Amplitude / V')
+plt.savefig('figures/histogram.pdf', dpi=300, bbox_inches='tight', transparent=True)
+plt.show()
+
+max_error = np.absolute(error).max()
+error_99th_percentile = np.percentile(error, 99)
+        
+print('The maxiumum reconstruction error is: {:0.2f}'.format(max_error))
+print('The 99th percentile of reconstruction error is: {:0.2f}'.format(error_99th_percentile))
